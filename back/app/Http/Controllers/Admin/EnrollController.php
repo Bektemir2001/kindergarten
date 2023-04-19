16fd8a06055f8a8cbaec1ca7361d37ac58fed027
amourhss <gazisimon702@gmail.com>
2023-04-19 21:56:57 +0600

    approve or deny an enrolled child functionality

--- back/app/Http/Controllers/Admin/EnrollController.php
+++ back/app/Http/Controllers/Admin/EnrollController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\UpdateChildrenRequest;
+use App\Models\Child;
 use App\Models\Enroll;
+use App\Models\Group;
+use Illuminate\Http\Request;
+
 
 class EnrollController
 {
     public function index(){
-        $enroll = Enroll::all();
-        return view('admin.enroll.index', compact('enroll'));
+        $enrolls = Enroll::all();
+        return view('admin.enroll.index', compact('enrolls'));
+    }
+
+    public function show(Enroll $enroll){
+        $groups = Group::all();
+        return view('admin.enroll.show', compact('enroll', 'groups'));
+    }
+
+    public function approve(Request $request, Enroll $enroll){
+        Child::create([
+            'name'=>$enroll['name'],
+            'surname' => $enroll['surname'],
+            'birth_date' => $enroll['birth_date'],
+            'parent_id' => $enroll['parent_id'],
+            'group_id'=>$request->groupId,
+            'photo' => $enroll['photo'],
+            'birth_certificate' => $enroll['birth_certificate'],
+            'med_certificate' => $enroll['med_certificate'],
+            'payment' => $enroll['payment'],
+        ]);
+        $enroll->delete();
+        return redirect()->route('admin.enroll.index')->with('status', 'Enrolling was approved.');
+    }
+
+    public function delete(Enroll $enroll){
+        $enroll->delete();
+        return redirect()->route('admin.enroll.index')->with('status', 'Enrolling was denied.');
     }
 }
--- back/resources/views/admin/enroll/show.blade.php
+++ back/resources/views/admin/enroll/show.blade.php
@@ -3,58 +3,61 @@
 
     <div class="content-wrapper">
         <div class="container mb-4 mt-4">
-
+            <form action="{{route('admin.enroll.approve', $enroll->id)}}" method="POST">
+                @csrf
             <div class="card card-primary card-outline">
                 <div class="card-body box-profile">
                     <ul class="list-group list-group-unbordered mb-3">
                         <li class="list-group-item">
                             <b>Name:</b>
-                            <div class="">{{$child->name}}</div>
+                            <div class="">{{$enroll->name}}</div>
                         </li>
                         <li class="list-group-item">
                             <b>Surname:</b>
-                            <div class="">{{$child->surname}}</div>
+                            <div class="">{{$enroll->surname}}</div>
                         </li>
                         <li class="list-group-item">
                             <b>Birth Date:</b>
-                            <div class="">{{$child->birth_date}}</div>
+                            <div class="">{{$enroll->birth_date}}</div>
                         </li>
                         <li class="list-group-item">
                             <b>Parent:</b>
-                            <div class="">{{$child->parent->name}} {{$child->parent->surname}}</div>
-                        </li>
-                        <li class="list-group-item">
-                            <b>Group Name:</b>
-                            <div class="">{{$child->group->name}}</div>
+                            <div class="">{{$enroll->parent->name}} {{$enroll->parent->surname}}</div>
                         </li>
                         <li class="list-group-item">
                             <b>Child's Photo:</b>
                             <div class="">
-                                <img class="img-fluid img" src="{{asset($child->photo)}}" alt="child's photo" style="width:70%;">
+                                <img class="img-fluid img" src="{{asset($enroll->photo)}}" alt="child's photo" style="width:70%;">
                             </div>
                         </li>
                         <li class="list-group-item">
                             <b>Birth Certificate:</b>
                             <div class="">
-                                <img class="img-fluid img" src="{{asset($child->birth_certificate)}}" alt="child's birth certificate" style="width:70%;">
+                                <img class="img-fluid img" src="{{asset($enroll->birth_certificate)}}" alt="child's birth certificate" style="width:70%;">
                             </div>
                         </li>
                         <li class="list-group-item">
                             <b>Medical Certificate:</b>
                             <div class="">
-                                <img class="img-fluid img" src="{{asset($child->med_certificate)}}" alt="child's medical certificate" style="width:70%;">
+                                <img class="img-fluid img" src="{{asset($enroll->med_certificate)}}" alt="child's medical certificate" style="width:70%;">
                             </div>
                         </li>
                         <li class="list-group-item">
-                            <b>Payment:</b>
-                            <div class="">{{$child->payment}}</div>
+                            <b>Groups:</b>
+                            <select class="form-control col-6" name="groupId">
+                            @foreach($groups as $group)
+                                <option value="{{$group->id}}">{{$group->name}}</option>
+                            @endforeach
+                            </select>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="modal-footer">
-                <a href="{{route('admin.children.index')}}" class="btn btn-secondary">Back</a>
+                <button type="submit" class="btn btn-success">Approve</button>
+                <a href="{{route('admin.enroll.index')}}" class="btn btn-secondary">Back</a>
             </div>
+            </form>
         </div>
     </div>
 

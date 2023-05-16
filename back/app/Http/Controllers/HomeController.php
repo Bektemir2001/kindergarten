dbb2e20c54919c4e692a2dde8cd4271b440eddb4
KanykeiKarataeva <kanysh150@gmail.com>
2023-05-17 05:30:25 +0600

    multilingual completed and content edit3

--- back/app/Http/Controllers/HomeController.php
+++ back/app/Http/Controllers/HomeController.php
@@ -62,6 +62,7 @@ class HomeController extends Controller
 
     public function condition(){
         return view('user.condition');
+    }
 
     public function faq(){
         $user = auth()->user();
